c1bd0ad3ef7d17d823aa21f9f488b28111224c61
madany044 <madanmadany2004@gmail.com>
2026-05-19 21:45:59 +0530

    fix port for deployment

--- app.py
+++ app.py
@@ -60,4 +60,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=DEBUG, port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=False, host="0.0.0.0", port=port)
